3a052107a80ece7d126339d97ce8d11b55fc4111
Aakash Verma <info.aakash11@gmail.com>
2021-11-20 23:23:08 +0530

    API setup done with routes & controllers

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,34 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config()
+const blogRoutes = require('./routes/blog')
 
 
 // creating app
 const app = express()
 
+// database setup
+mongoose.connect('mongodb://localhost:27017/blog').then(() => console.log("DB Connected"));
+
+// mongoose
+// .connect("mongodb://localhost:27017/blog", {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false})
+// .then(() => console.log('DB Connected'));
+ 
 // using middlewares
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
+// routes middleware
+app.use('/api', blogRoutes)
+
 // handling cors
-app.use(cors())
+if (process.env.NODE_ENV == 'development') {
+    app.use(cors({origin: `${process.env.CLIENT_URL}`}))
+}
+
 
 // routes
 app.get("/api", (req, res) => {
